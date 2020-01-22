0ab1e6ba9e3727c2f9180fd09644dd371a0ca8ec
Guillaume R <guillaume.rondon@gmail.com>
2020-01-22 07:43:51 +0100

    added a function to count jobs by id

--- lib/wttjelixir.ex
+++ lib/wttjelixir.ex
@@ -17,9 +17,15 @@ defmodule Wttjelixir do
   end
 
 
-  def mapcsv do
+  def mapcsvjobs do
     File.read!("data/technical-test-jobs.csv")
     |> String.split("\n")
     |> Enum.map(&String.split(&1, ","))
+    |> Enum.drop(1)
+  end
+
+  def countjobsbyid(jobid) do
+    mapcsvjobs()
+    |> Enum.count(&(Enum.at(&1,0)  =~ jobid))
   end
 end
--- test/wttjelixir_test.exs
+++ test/wttjelixir_test.exs
@@ -6,7 +6,12 @@ defmodule WttjelixirTest do
     assert Wttjelixir.hello() == :world
   end
 
-  test "reads csv and return a non empty list of jobs" do
-    assert length(Wttjelixir.mapcsv()) > 0
+  test "reads the csv and returns the list of jobs" do
+    assert length(Wttjelixir.mapcsvjobs()) == 5070
+    assert Enum.at(Wttjelixir.mapcsvjobs(), 0) == ["7", "INTERNSHIP", "[Louis Vuitton Germany] Praktikant (m/w) im Bereich Digital Retail (E-Commerce)", "48.1392154", "11.5781413"]
+  end
+
+  test "counts nb jobs for id 22" do
+    assert Wttjelixir.countjobsbyid("22") == 30
   end
 end
