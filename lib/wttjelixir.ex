e2a10210a62fd82b133f8d6ce1643e6662d9d9c9
Guillaume R <guillaume.rondon@gmail.com>
2020-01-22 20:20:25 +0100

    calculate sum by contract type, trying to replace job id by category

--- lib/wttjelixir.ex
+++ lib/wttjelixir.ex
@@ -16,16 +16,14 @@ defmodule Wttjelixir do
     :world
   end
 
-  def filepath do
-    "data/"
-  end
+  @filepath "data/"
 
   def professions_csv do
-    filepath() <> "technical-test-professions.csv"
-  end
-  
+    @filepath <> "technical-test-professions.csv"
+  end 
+
   def jobs_csv do
-    filepath() <> "technical-test-jobs.csv"
+    @filepath <> "technical-test-jobs.csv"
   end
 
   def map_csv(filename) do
@@ -35,8 +33,30 @@ defmodule Wttjelixir do
     |> Enum.drop(1)
   end
 
-  def count_jobs_by_id(jobid) do
-    map_csv(jobs_csv())
-    |> Enum.count(&(Enum.at(&1,0)  =~ jobid))
+  def count_jobs_by_id(joblist, jobid) do
+    Enum.count(joblist, &(Enum.at(&1,0)  =~ jobid))
+  end
+
+  def group_jobs_by_type(joblist) do
+    Enum.group_by(Enum.map(joblist, &([List.first(&1),Enum.at(&1,1)])), &(Enum.at(&1,1)))
+  end
+
+  def get_category_by_job(professions, job_id) do 
+    List.last(List.last(Enum.filter(professions, fn(x) ->
+       Enum.at(x,0)== job_id 
+      end)))
+  end
+
+  def replace_jobid_by_category(joblist, professions) do
+    Enum.map(joblist, fn(x) ->
+      List.replace_at(x,0, Wttjelixir.get_category_by_job(professions, Enum.at(x, 0))) 
+    end)
+  end
+
+  def output_nb_job_by_category(joblist) do
+    map = Enum.group_by(Enum.map(joblist, &([List.first(&1),Enum.at(&1,1)])), &(Enum.at(&1,1)))
+    Enum.each(map, fn ({key, value}) ->   
+      IO.puts("#{key}: #{length(value)}") 
+    end)
   end
 end
--- test/wttjelixir_test.exs
+++ test/wttjelixir_test.exs
@@ -1,5 +1,7 @@
 defmodule WttjelixirTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
+
   doctest Wttjelixir
 
   test "greets the world" do
@@ -7,21 +9,56 @@ defmodule WttjelixirTest do
   end
 
   test "reads the csv and returns the list of jobs" do
-    assert length(Wttjelixir.map_csv(Wttjelixir.jobs_csv())) == 5070
-    assert Enum.at(Wttjelixir.map_csv(Wttjelixir.jobs_csv()), 0) == ["7", "INTERNSHIP", "[Louis Vuitton Germany] Praktikant (m/w) im Bereich Digital Retail (E-Commerce)", "48.1392154", "11.5781413"]
+    jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
+    assert length(jobs) >= 1
+    assert is_list(Enum.at(jobs, 0)) #["7", "INTERNSHIP", "[Louis Vuitton Germany] Praktikant (m/w) im Bereich Digital Retail (E-Commerce)", "48.1392154", "11.5781413"]
   end
-
+  
   test "counts nb jobs for a given id" do
-    assert Wttjelixir.count_jobs_by_id("22") == 30
+    jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
+    assert Wttjelixir.count_jobs_by_id(jobs, "22") >= 1
   end
 
   test "counts zero jobs for an unknown job id" do
-    assert Wttjelixir.count_jobs_by_id("999") == 0
+    jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
+    assert Wttjelixir.count_jobs_by_id(jobs, "AAA") == 0
   end
 
   test "reads the csv and returns the list of professions" do
-    assert length(Wttjelixir.map_csv(Wttjelixir.professions_csv())) == 42
-    assert Enum.at(Wttjelixir.map_csv(Wttjelixir.professions_csv()), 0) == ["17","Devops / Infrastructure","Tech"]
+    professions = Wttjelixir.map_csv(Wttjelixir.professions_csv())
+    assert length(professions) >= 1
+    assert is_list(Enum.at(professions, 0)) #["17","Devops / Infrastructure","Tech"]
+  end
+
+  test "groups jobs by type of contract" do
+    jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
+    assert length(Map.fetch!(Wttjelixir.group_jobs_by_type(jobs), "APPRENTICESHIP"))  >= 1
+  end
+
+  test "displays the sum of job by type of contract" do
+    jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
+    assert capture_io(fn ->
+      Wttjelixir.output_nb_job_by_category(jobs)
+    end) == """
+    APPRENTICESHIP: 423
+    FREELANCE: 38
+    FULL_TIME: 3091
+    INTERNSHIP: 1292
+    PART_TIME: 4
+    TEMPORARY: 211
+    VIE: 10
+    """
+  end
+  
+  test "matches a job category for a given job" do
+    professions = Wttjelixir.map_csv(Wttjelixir.professions_csv())
+    assert Wttjelixir.get_category_by_job(professions, "17") == "Tech"
+  end
+
+  test "remplace le job id par sa categorie" do
+    jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
+    professions = Wttjelixir.map_csv(Wttjelixir.professions_csv())
+    assert List.first(Enum.at(Wttjelixir.replace_jobid_by_category(jobs,professions),0)) == "Marketing / Comm'"
   end
 
 
