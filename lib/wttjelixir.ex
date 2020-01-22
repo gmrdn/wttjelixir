aa2193a0617c84e50caf03e96e1d61bb8de7ecb4
Guillaume R <guillaume.rondon@gmail.com>
2020-01-22 21:48:46 +0100

    added counts by type and category as a list of list

--- lib/wttjelixir.ex
+++ lib/wttjelixir.ex
@@ -34,29 +34,67 @@ defmodule Wttjelixir do
   end
 
   def count_jobs_by_id(joblist, jobid) do
-    Enum.count(joblist, &(Enum.at(&1,0)  =~ jobid))
-  end
-
-  def group_jobs_by_type(joblist) do
-    Enum.group_by(Enum.map(joblist, &([List.first(&1),Enum.at(&1,1)])), &(Enum.at(&1,1)))
-  end
-
-  def get_category_by_job(professions, job_id) do 
-    List.last(List.last(Enum.filter(professions, fn(x) ->
-       Enum.at(x,0)== job_id 
-      end)))
-  end
-
-  def replace_jobid_by_category(joblist, professions) do
-    Enum.map(joblist, fn(x) ->
-      List.replace_at(x,0, Wttjelixir.get_category_by_job(professions, Enum.at(x, 0))) 
+    Enum.count(joblist, fn(x) -> 
+      Enum.at(x,0)  =~ jobid
     end)
   end
 
-  def output_nb_job_by_category(joblist) do
-    map = Enum.group_by(Enum.map(joblist, &([List.first(&1),Enum.at(&1,1)])), &(Enum.at(&1,1)))
+  def group_jobs_by_type(joblist) do
+    joblist
+    |> Enum.map(fn(x) -> 
+      [List.first(x), Enum.at(x,1)]
+    end)
+    |> Enum.group_by(fn(x) -> 
+      Enum.at(x,1)
+    end)  
+  end
+
+  def get_category_by_job(professions, job_id) do 
+    professions
+    |> Enum.filter(fn(x) ->
+      Enum.at(x,0) == job_id
+    end)
+    |> List.last()
+    |> List.last()
+  end
+
+  def replace_jobid_by_category(joblist, professions) do
+    joblist
+    |> Enum.map(fn(x) ->
+      List.replace_at(x,0, get_category_by_job(professions, Enum.at(x, 0))) 
+    end)
+  end
+
+  def output_nb_job_by_type(joblist) do
+    map = group_jobs_by_type(joblist)
     Enum.each(map, fn ({key, value}) ->   
       IO.puts("#{key}: #{length(value)}") 
     end)
   end
+
+  def group_jobs_by_type_and_category(joblist) do
+    joblist
+    |> Enum.map(fn(x) -> 
+      [List.first(x), Enum.at(x,1)]
+    end)
+    |> Enum.group_by(fn(x) -> 
+      {Enum.at(x,1), Enum.at(x,0)}
+    end)
+  end
+
+  def output_nb_job_by_type_and_category(joblist) do
+    Enum.each(joblist, fn ({key, value}) ->   
+      IO.puts("#{Enum.join(Tuple.to_list(key), " ")}: #{length(value)}") 
+    end)
+  end
+
+  def count_jobs_by_type_and_category(jobs, professions) do
+    jobs_replaced = replace_jobid_by_category(jobs,professions)
+    map_grouped = group_jobs_by_type_and_category(jobs_replaced)
+
+    Enum.sort(Enum.map(map_grouped, fn ({key, value}) ->   
+      [Tuple.to_list(key), length(value)] 
+    end))
+  end
+ 
 end
--- test/wttjelixir_test.exs
+++ test/wttjelixir_test.exs
@@ -38,7 +38,7 @@ defmodule WttjelixirTest do
   test "displays the sum of job by type of contract" do
     jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
     assert capture_io(fn ->
-      Wttjelixir.output_nb_job_by_category(jobs)
+      Wttjelixir.output_nb_job_by_type(jobs)
     end) == """
     APPRENTICESHIP: 423
     FREELANCE: 38
@@ -55,10 +55,27 @@ defmodule WttjelixirTest do
     assert Wttjelixir.get_category_by_job(professions, "17") == "Tech"
   end
 
-  test "remplace le job id par sa categorie" do
+  test "replace the job id by its category" do
     jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
     professions = Wttjelixir.map_csv(Wttjelixir.professions_csv())
     assert List.first(Enum.at(Wttjelixir.replace_jobid_by_category(jobs,professions),0)) == "Marketing / Comm'"
+  end
+
+  test "groups jobs by type of contract and by category" do
+    jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
+    professions = Wttjelixir.map_csv(Wttjelixir.professions_csv())
+    jobs_replaced = Wttjelixir.replace_jobid_by_category(jobs,professions)
+    map_grouped = Wttjelixir.group_jobs_by_type_and_category(jobs_replaced)
+    assert length(Map.fetch!(map_grouped, {"FULL_TIME", "Conseil"}))  >= 1
+  end
+
+  test "counts the number of job by type of contract and job category" do
+    jobs = Wttjelixir.map_csv(Wttjelixir.jobs_csv())
+    professions = Wttjelixir.map_csv(Wttjelixir.professions_csv())
+    table_counts = Wttjelixir.count_jobs_by_type_and_category(jobs, professions) 
+    assert length(table_counts) >= 1
+    assert List.first(table_counts) == [["APPRENTICESHIP", ""], 4]
+    assert List.last(table_counts) == [["VIE", "Tech"], 1]
   end
 
 
